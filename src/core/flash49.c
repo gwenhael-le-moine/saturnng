gwenhael-le-moine/saturnng

commit 97371cf37289d81fc87ca74f68bcaa8940cd0d76

In which I refactor code so that it makes a bit more sense to me.
Do 1 thing per line

src/core/flash49.c
@@ -192,6 +192,7 @@ static int ReadXSR( enum FlashState* state, enum FlashCycle cycle, XAddress addr
        emulation scheme.  Keep current state.
     */
     debug1( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM_OP, "Read XSR" );
+
     return FLASH_XSR_WBS;
 }
 
@@ -202,6 +203,7 @@ static int ReadSR( enum FlashState* state, enum FlashCycle cycle, XAddress addre
        emulation scheme.  Keep current state.
     */
     debug1( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM_OP, "Read SR" );
+
     return FLASH_SR_WSMS;
 }
 
@@ -217,6 +219,7 @@ static int StoreCount( enum FlashState* state, enum FlashCycle cycle, XAddress a
     wb_count = wb_cdown = data & WB_COUNT_MASK;
 
     *state = FLASH_ST_WRITE_DATA_1;
+
     return 0; /* No result; this is a write cycle */
 }
 
@@ -227,8 +230,6 @@ static int StoreCount( enum FlashState* state, enum FlashCycle cycle, XAddress a
 */
 static int StoreData( enum FlashState* state, enum FlashCycle cycle, XAddress address, int data )
 {
-    int index;
-
     debug1( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM_OP, "Write to Buffer (data)" );
 
     /* Store WRITE_BUFFER data; the first write also stores the
@@ -243,12 +244,15 @@ static int StoreData( enum FlashState* state, enum FlashCycle cycle, XAddress ad
             break;
 
         case FLASH_ST_WRITE_DATA_N:
-            index = address - wb_start;
-            if ( index < 0 || index >= WB_SIZE ) {
-                ChfGenerate( FLASH_CHF_MODULE_ID, __FILE__, __LINE__, FLASH_W_BAD_ADDRESS, CHF_WARNING, *state, cycle, address, data );
-                ChfSignal( FLASH_CHF_MODULE_ID );
-            } else
-                wb[ index ] = data;
+            {
+                int index = address - wb_start;
+                if ( index <= 0 && index < WB_SIZE )
+                    wb[ index ] = data;
+                else {
+                    ChfGenerate( FLASH_CHF_MODULE_ID, __FILE__, __LINE__, FLASH_W_BAD_ADDRESS, CHF_WARNING, *state, cycle, address, data );
+                    ChfSignal( FLASH_CHF_MODULE_ID );
+                }
+            }
             break;
 
         default:
@@ -273,18 +277,17 @@ static int WriteConfirm( enum FlashState* state, enum FlashCycle cycle, XAddress
 
     /* Expect Write to Buffer confirmation code */
     if ( data == FLASH_CMD_WRITE_BUFFER_2 ) {
-        int i;
-
         /* Confirmation OK; write.
            Remember that wb_count is the byte count MINUS 1.
         */
-        for ( i = 0; i <= wb_count; i++ ) {
+        for ( int i = 0; i <= wb_count; i++ ) {
             mod_status_49->flash[ NibbleAddress( wb_start + i ) ] = LowNibble( wb[ i ] );
             mod_status_49->flash[ NibbleAddress( wb_start + i ) + 1 ] = HighNibble( wb[ i ] );
         }
     }
 
     *state = FLASH_ST_READ_ARRAY;
+
     return 0; /* No result */
 }
 
@@ -300,16 +303,16 @@ static int BlockErase( enum FlashState* state, enum FlashCycle cycle, XAddress a
     /* Expect Write to Buffer confirmation code */
     if ( data == FLASH_CMD_BL_ERASE_2 ) {
         XAddress block_base = BlockBase( address );
-        int i;
 
         /* Confirmation OK; erase */
-        for ( i = 0; i < BLOCK_SIZE; i++ ) {
+        for ( int i = 0; i < BLOCK_SIZE; i++ ) {
             mod_status_49->flash[ NibbleAddress( block_base + i ) ] = ( Nibble )0xF;
             mod_status_49->flash[ NibbleAddress( block_base + i ) + 1 ] = ( Nibble )0xF;
         }
     }
 
     *state = FLASH_ST_READ_SR;
+
     return 0; /* No result */
 }
 
@@ -339,14 +342,13 @@ static FlashF F[ FLASH_ST_N ][ FLASH_CYCLE_N ] = {
 */
 static int FSM( enum FlashCycle cycle, XAddress address, int data )
 {
-    int result;
-
     debug2( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM, fsm_state, cycle );
     debug2( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM_AD, address, data );
 
-    result = F[ fsm_state ][ cycle ]( &fsm_state, cycle, address, data );
+    int result = F[ fsm_state ][ cycle ]( &fsm_state, cycle, address, data );
 
     debug2( FLASH_CHF_MODULE_ID, DEBUG_C_FLASH, FLASH_I_FSM_RESULT, fsm_state, result );
+
     return result;
 }
 

src/core/emulator.c
@@ -503,9 +503,9 @@ void Emulator( void )
 
         /* Activate emulator loop */
         EmulatorLoop();
-    } /*  else { */
-    /*     /\* Unwinding after an emulator interrupt *\/ */
-    /* } */
+    } else {
+        /* Unwinding after an emulator interrupt */
+    }
 }
 
 /* .+

src/core/hdw.c
@@ -132,7 +132,8 @@ void HdwInit( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "HdwInit" );
 
-    if ( ReadStructFromFile( config.hdw_path, sizeof( mod_status.hdw ), &mod_status.hdw ) ) {
+    bool err = ReadStructFromFile( config.hdw_path, sizeof( mod_status.hdw ), &mod_status.hdw );
+    if ( err ) {
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_HDW_INIT, CHF_WARNING );
         ChfSignal( MOD_CHF_MODULE_ID );
 
@@ -164,7 +165,8 @@ void HdwSave( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "HdwSave" );
 
-    if ( WriteStructToFile( &mod_status.hdw, sizeof( mod_status.hdw ), config.hdw_path ) ) {
+    bool err = WriteStructToFile( &mod_status.hdw, sizeof( mod_status.hdw ), config.hdw_path );
+    if ( err ) {
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_HDW_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
     }

src/core/modules.c
@@ -395,11 +395,11 @@ static struct ModMap* ClearCachingInfo( struct ModMap* d )
 .- */
 static struct ModMap* NewModMap( void )
 {
-    struct ModMap* new;
-
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "NewModMap" );
 
-    if ( ( new = ( struct ModMap* )malloc( sizeof( struct ModMap ) ) ) == ( struct ModMap* )NULL ) {
+    struct ModMap* new = ( struct ModMap* )malloc( sizeof( struct ModMap ) );
+
+    if ( new == ( struct ModMap* )NULL ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_F_MAP_ALLOC, CHF_FATAL );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -446,6 +446,7 @@ static struct ModMap* CopyModMap( struct ModMap* d, const struct ModMap* s )
 
     *d = *s;
     d->cache.link = link; /* Restore .link */
+
     return ClearCachingInfo( d );
 }
 
@@ -577,11 +578,9 @@ static void FlushCache( struct ModMap* save )
 .- */
 static struct ModMap* AccessConfigCache( Address tag )
 {
-    int i;
-
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "AccessConfigCache" );
 
-    for ( i = 0; i < N_MOD_CACHE_ENTRIES; i++ )
+    for ( int i = 0; i < N_MOD_CACHE_ENTRIES; i++ )
         if ( MOD_MAP.cache.config[ i ].tag == tag )
             return MOD_MAP.cache.config[ i ].map_ptr;
 
@@ -1266,12 +1265,12 @@ void ModConfig( Address config_info )
 void ModUnconfig( Address unconfig_info )
 {
     struct ModMap *nxt, *old;
-    int mod;
+    int mod = MOD_MAP.page_table[ ModPage( unconfig_info ) ].index;
 
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "ModUnconfig" );
 
     /* Determine the module to unconfigure */
-    if ( ( mod = MOD_MAP.page_table[ ModPage( unconfig_info ) ].index ) == MOD_NO_MOD_INDEX ) {
+    if ( mod == MOD_NO_MOD_INDEX ) {
         /* There isn't any module configured at the given address -
            Signal a warning
         */
@@ -1486,18 +1485,14 @@ void WriteNibble( Address addr, Nibble datum )
 .- */
 void ModMapCheck( Address addr, char ob[ MOD_MAP_CHECK_OB_SIZE ] )
 {
-    int page;
-    Address offset;
-    int mod;
+    int page = ModPage( addr );
+    Address offset = ModOffset( addr );
+    int mod = MOD_MAP.page_table[ page ].index;
 
-    page = ModPage( addr );
-    offset = ModOffset( addr );
-
-    if ( ( mod = MOD_MAP.page_table[ page ].index ) == MOD_NO_MOD_INDEX )
+    if ( mod == MOD_NO_MOD_INDEX )
         sprintf( ob, "A[%05X] -> *Not Mapped*", addr );
     else {
-        Address rel_addr;
-        rel_addr = MOD_MAP.page_table[ page ].rel_base_addr | offset;
+        Address rel_addr = MOD_MAP.page_table[ page ].rel_base_addr | offset;
 
         sprintf( ob, "A[%05X] -> M[%s] R[%05X]", addr, mod_description[ mod ].name, rel_addr );
     }
@@ -1526,12 +1521,10 @@ void ModMapCheck( Address addr, char ob[ MOD_MAP_CHECK_OB_SIZE ] )
 .- */
 void ModMapTable( char ob[ MOD_MAP_TABLE_OB_SIZE ] )
 {
-    int mod;
-
     sprintf( ob, "%s\n", "Device\t\t\tAddress\tSize\tStatus" );
     ob += strlen( ob );
 
-    for ( mod = 0; mod < N_MOD; mod++ ) {
+    for ( int mod = 0; mod < N_MOD; mod++ ) {
         sprintf( ob, "%s\t%05X\t%05X\t%s", mod_description[ mod ].name, MOD_MAP.map_info[ mod ].abs_base_addr, MOD_MAP.map_info[ mod ].size,
                  MOD_MAP.map_info[ mod ].config == MOD_CONFIGURED
                      ? "Configured"

src/core/monitor.c
@@ -94,9 +94,11 @@ static int ReadHexAddress( Address* addr )
 static int ReadHexDatum( Nibble* n )
 {
     Address addr;
-    int st;
-    if ( ( st = ReadHexAddress( &addr ) ) == OK )
+    int st = ReadHexAddress( &addr );
+
+    if ( st == OK )
         *n = ( Nibble )addr;
+
     return st;
 }
 
@@ -104,6 +106,7 @@ static int ReadHexDatum( Nibble* n )
 static int ReadCount( int* count )
 {
     char* p = strtok( ( char* )NULL, TOK_DELIMITERS );
+
     return ( p == ( char* )NULL || sscanf( p, COUNT_FMT, count ) != 1 || *count <= 0 ) ? FAILED : OK;
 }
 
@@ -117,6 +120,7 @@ static int ReadCount( int* count )
 static int run( void )
 {
     Emulator();
+
     return OK;
 }
 
@@ -124,9 +128,13 @@ static int run( void )
 static int debug( void )
 {
     Address addr;
-    if ( ReadHexAddress( &addr ) )
+    int st = ReadHexAddress( &addr );
+
+    if ( st )
         return FAILED;
+
     config.debug_level |= ( int )addr;
+
     return OK;
 }
 
@@ -134,11 +142,15 @@ static int debug( void )
 static int map_check( void )
 {
     Address addr;
-    char ob[ MOD_MAP_CHECK_OB_SIZE ];
-    if ( ReadHexAddress( &addr ) )
+    int st = ReadHexAddress( &addr );
+
+    if ( st )
         return FAILED;
+
+    char ob[ MOD_MAP_CHECK_OB_SIZE ];
     ModMapCheck( addr, ob );
     puts( ob );
+
     return OK;
 }
 
@@ -148,6 +160,7 @@ static int map( void )
     char ob[ MOD_MAP_TABLE_OB_SIZE ];
     ModMapTable( ob );
     puts( ob );
+
     return OK;
 }
 
@@ -168,8 +181,11 @@ static int r( void )
 {
     Address addr;
     int count;
-    if ( ReadHexAddress( &addr ) )
+    int st = ReadHexAddress( &addr );
+
+    if ( st )
         return FAILED;
+
     if ( ReadCount( &count ) )
         count = 1;
 
@@ -188,8 +204,11 @@ static int d( void )
     int count;
     char ob[ DISASSEMBLE_OB_SIZE ];
 
-    if ( ReadHexAddress( &addr ) )
+    int st = ReadHexAddress( &addr );
+
+    if ( st )
         return FAILED;
+
     if ( ReadCount( &count ) )
         count = 1;
 
@@ -207,6 +226,7 @@ static int cpu( void )
     char ob[ DUMP_CPU_STATUS_OB_SIZE ];
     DumpCpuStatus( ob );
     puts( ob );
+
     return OK;
 }
 
@@ -214,6 +234,7 @@ static int cpu( void )
 static int reset( void )
 {
     CpuReset();
+
     return OK;
 }
 
@@ -223,6 +244,7 @@ static int mon_exit( void )
     ModSave();
     CpuSave();
     exit( EXIT_SUCCESS );
+
     return OK; /* 3.1: Keep compiler happy */
 }
 
@@ -230,6 +252,7 @@ static int mon_exit( void )
 static int mon_quit( void )
 {
     exit( EXIT_SUCCESS );
+
     return OK; /* 3.1: Keep compiler happy */
 }
 
@@ -310,11 +333,8 @@ static void sigint_handler( int s ) { EmulatorIntRequest(); }
 void Monitor( void )
 {
     char cmd[ LINE_BUFFER_SIZE ];
-    char old_cmd[ LINE_BUFFER_SIZE ];
     char* tk;
-
-    /* Clear old_cmd buffer */
-    strcpy( old_cmd, "" );
+    char* ret;
 
     /* Establish SIGINT handler */
     signal( SIGINT, sigint_handler );
@@ -325,21 +345,18 @@ void Monitor( void )
         fputs( PROMPT, stdout );
         fflush( stdout );
 
-        if ( fgets( cmd, LINE_BUFFER_SIZE, stdin ) == ( char* )NULL || ( tk = strtok( cmd, TOK_DELIMITERS ) ) == ( char* )NULL ) {
-            /* New command empty; try old command */
-            if ( ( tk = strtok( old_cmd, TOK_DELIMITERS ) ) != ( char* )NULL )
-                if ( InvokeCommand( tk ) )
-                    ChfGenerate( CPU_CHF_MODULE_ID, __FILE__, __LINE__, CPU_W_BAD_MONITOR_CMD, CHF_WARNING, tk );
-            ChfSignal( CPU_CHF_MODULE_ID );
-        } else {
-            /* Save command */
-            strcpy( old_cmd, cmd );
+        ret = fgets( cmd, LINE_BUFFER_SIZE, stdin );
+        if ( ret == ( char* )NULL )
+            continue;
 
-            /* New command */
-            if ( InvokeCommand( tk ) ) {
-                ChfGenerate( CPU_CHF_MODULE_ID, __FILE__, __LINE__, CPU_W_BAD_MONITOR_CMD, CHF_WARNING, tk );
-                ChfSignal( CPU_CHF_MODULE_ID );
-            }
+        tk = strtok( cmd, TOK_DELIMITERS );
+        if ( tk == ( char* )NULL )
+            continue;
+
+        bool err = InvokeCommand( tk );
+        if ( err ) {
+            ChfGenerate( CPU_CHF_MODULE_ID, __FILE__, __LINE__, CPU_W_BAD_MONITOR_CMD, CHF_WARNING, tk );
+            ChfSignal( CPU_CHF_MODULE_ID );
         }
     }
 }

src/core/romram.c
@@ -123,15 +123,18 @@ void RomInit( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RomInit" );
 
-    if ( ( mod_status_48 = ( struct ModStatus_48* )malloc( sizeof( struct ModStatus_48 ) ) ) == ( struct ModStatus_48* )NULL ) {
+    mod_status_48 = ( struct ModStatus_48* )malloc( sizeof( struct ModStatus_48 ) );
+    if ( mod_status_48 == ( struct ModStatus_48* )NULL ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_F_MOD_STATUS_ALLOC, CHF_FATAL, sizeof( struct ModStatus_48 ) );
         ChfSignal( MOD_CHF_MODULE_ID );
     }
 
-    if ( ReadNibblesFromFile( config.rom_path, N_ROM_SIZE, mod_status_rom ) ) {
+    bool err = ReadNibblesFromFile( config.rom_path, N_ROM_SIZE, mod_status_rom );
+    if ( err ) {
         // To load 48SX ROM, try again with half the size this time.
-        if ( ReadNibblesFromFile( config.rom_path, N_ROM_SIZE / 2, mod_status_rom ) ) {
+        err = ReadNibblesFromFile( config.rom_path, N_ROM_SIZE / 2, mod_status_rom );
+        if ( err ) {
             ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_F_ROM_INIT, CHF_FATAL );
             ChfSignal( MOD_CHF_MODULE_ID );
         }
@@ -242,7 +245,8 @@ void RamInit( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RamInit" );
 
-    if ( ReadNibblesFromFile( config.ram_path, N_RAM_SIZE, mod_status_ram ) ) {
+    bool err = ReadNibblesFromFile( config.ram_path, N_RAM_SIZE, mod_status_ram );
+    if ( err ) {
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_RAM_INIT, CHF_WARNING );
         ChfSignal( MOD_CHF_MODULE_ID );
 
@@ -275,7 +279,8 @@ void RamSave( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RamSave" );
 
-    if ( WriteNibblesToFile( mod_status_ram, N_RAM_SIZE, config.ram_path ) ) {
+    bool err = WriteNibblesToFile( mod_status_ram, N_RAM_SIZE, config.ram_path );
+    if ( err ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_RAM_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -501,14 +506,8 @@ void Ce2Init( void )
 
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "Ce2Init" );
 
-    if ( ReadNibblesFromFile( config.port1_path, N_PORT_1_SIZE, mod_status_port_1 ) ) {
-        ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_PORT_1_INIT, CHF_WARNING );
-        ChfSignal( MOD_CHF_MODULE_ID );
-
-        ( void )memset( mod_status_port_1, 0, sizeof( mod_status_port_1 ) );
-
-        new_status = mod_status_hdw.card_status & ~( CE2_CARD_PRESENT | CE2_CARD_WE );
-    } else {
+    bool err = ReadNibblesFromFile( config.port1_path, N_PORT_1_SIZE, mod_status_port_1 );
+    if ( !err ) {
         /* Card present; check write protection */
         new_status = mod_status_hdw.card_status | CE2_CARD_PRESENT;
 
@@ -521,6 +520,13 @@ void Ce2Init( void )
             ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_I_PORT_1_WP, CHF_INFO );
             ChfSignal( MOD_CHF_MODULE_ID );
         }
+    } else {
+        ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_PORT_1_INIT, CHF_WARNING );
+        ChfSignal( MOD_CHF_MODULE_ID );
+
+        ( void )memset( mod_status_port_1, 0, sizeof( mod_status_port_1 ) );
+
+        new_status = mod_status_hdw.card_status & ~( CE2_CARD_PRESENT | CE2_CARD_WE );
     }
 
     if ( new_status != mod_status_hdw.card_status ) {
@@ -559,7 +565,11 @@ void Ce2Save( void )
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "Ce2Save" );
 
     /* Attempt to save only if port is write-enabled */
-    if ( ( mod_status_hdw.card_status & CE2_CARD_WE ) && WriteNibblesToFile( mod_status_port_1, N_PORT_1_SIZE, config.port1_path ) ) {
+    if ( !( mod_status_hdw.card_status & CE2_CARD_WE ) )
+        return;
+
+    bool err = WriteNibblesToFile( mod_status_port_1, N_PORT_1_SIZE, config.port1_path );
+    if ( err ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_PORT_1_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -652,14 +662,8 @@ void NCe3Init( void )
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "NCe3Init" );
 
 #ifdef N_PORT_2_BANK
-    if ( ReadNibblesFromFile( config.port2_path, N_PORT_2_SIZE, mod_status_port_2 ) ) {
-        ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_PORT_2_INIT, CHF_WARNING );
-        ChfSignal( MOD_CHF_MODULE_ID );
-
-        ( void )memset( mod_status_port_2, 0, sizeof( mod_status_port_2 ) );
-
-        new_status = mod_status_hdw.card_status & ~( NCE3_CARD_PRESENT | NCE3_CARD_WE );
-    } else {
+    bool err = ReadNibblesFromFile( config.port2_path, N_PORT_2_SIZE, mod_status_port_2 );
+    if ( !err ) {
         /* Card present; check write protection */
         new_status = mod_status_hdw.card_status | NCE3_CARD_PRESENT;
 
@@ -672,6 +676,13 @@ void NCe3Init( void )
             ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_I_PORT_2_WP, CHF_INFO );
             ChfSignal( MOD_CHF_MODULE_ID );
         }
+    } else {
+        ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_PORT_2_INIT, CHF_WARNING );
+        ChfSignal( MOD_CHF_MODULE_ID );
+
+        ( void )memset( mod_status_port_2, 0, sizeof( mod_status_port_2 ) );
+
+        new_status = mod_status_hdw.card_status & ~( NCE3_CARD_PRESENT | NCE3_CARD_WE );
     }
 
 #else
@@ -716,7 +727,11 @@ void NCe3Save( void )
 
 #ifdef N_PORT_2_BANK
     /* Attempt to save only if port is write-enabled */
-    if ( ( mod_status_hdw.card_status & NCE3_CARD_WE ) && WriteNibblesToFile( mod_status_port_2, N_PORT_2_SIZE, config.port2_path ) ) {
+    if ( !( mod_status_hdw.card_status & NCE3_CARD_WE ) )
+        return;
+
+    bool err = WriteNibblesToFile( mod_status_port_2, N_PORT_2_SIZE, config.port2_path );
+    if ( err ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_PORT_2_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -751,12 +766,11 @@ Nibble NCe3Read( Address rel_address )
 
 #ifdef N_PORT_2_BANK
     return mod_status_port_2[ rel_address | mod_status_hdw.accel.a48.bs_address ];
-
 #else
     ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_NCE3_READ, CHF_ERROR, rel_address );
     ChfSignal( MOD_CHF_MODULE_ID );
-    return ( Nibble )0;
 
+    return ( Nibble )0;
 #endif
 }
 

src/core/romram49.c
@@ -137,13 +137,15 @@ void RomInit49( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RomInit49" );
 
-    if ( ( mod_status_49 = ( struct ModStatus_49* )malloc( sizeof( struct ModStatus_49 ) ) ) == ( struct ModStatus_49* )NULL ) {
+    mod_status_49 = ( struct ModStatus_49* )malloc( sizeof( struct ModStatus_49 ) );
+    if ( mod_status_49 == ( struct ModStatus_49* )NULL ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_F_MOD_STATUS_ALLOC, CHF_FATAL, sizeof( struct ModStatus_49 ) );
         ChfSignal( MOD_CHF_MODULE_ID );
     }
 
-    if ( ReadNibblesFromFile( config.rom_path, N_FLASH_SIZE_49, mod_status_49->flash ) ) {
+    bool err = ReadNibblesFromFile( config.rom_path, N_FLASH_SIZE_49, mod_status_49->flash );
+    if ( err ) {
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_F_ROM_INIT, CHF_FATAL );
         ChfSignal( MOD_CHF_MODULE_ID );
     }
@@ -173,7 +175,8 @@ void RomSave49( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RomSave49" );
 
-    if ( WriteNibblesToFile( mod_status_49->flash, N_FLASH_SIZE_49, config.rom_path ) ) {
+    bool err = WriteNibblesToFile( mod_status_49->flash, N_FLASH_SIZE_49, config.rom_path );
+    if ( err ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_ROM_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -201,11 +204,9 @@ void RomSave49( void )
 .- */
 Nibble RomRead49( Address rel_address )
 {
-    register XAddress view;
-
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RomRead49" );
 
-    view = mod_status.hdw.accel.a49.view[ ( rel_address & FLASH_VIEW_SELECTOR ) != 0 ];
+    register XAddress view = mod_status.hdw.accel.a49.view[ ( rel_address & FLASH_VIEW_SELECTOR ) != 0 ];
 
     return mod_status_49->flash[ view | ( rel_address & FLASH_BANK_MASK ) ];
 }
@@ -272,7 +273,8 @@ void RamInit49( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RamInit49" );
 
-    if ( ReadNibblesFromFile( config.ram_path, N_RAM_SIZE_49, mod_status_49->ram ) ) {
+    bool err = ReadNibblesFromFile( config.ram_path, N_RAM_SIZE_49, mod_status_49->ram );
+    if ( err ) {
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_W_RAM_INIT, CHF_WARNING );
         ChfSignal( MOD_CHF_MODULE_ID );
 
@@ -303,7 +305,8 @@ void RamSave49( void )
 {
     debug1( MOD_CHF_MODULE_ID, DEBUG_C_TRACE, MOD_I_CALLED, "RamSave49" );
 
-    if ( WriteNibblesToFile( mod_status_49->ram, N_RAM_SIZE_49, config.ram_path ) ) {
+    bool err = WriteNibblesToFile( mod_status_49->ram, N_RAM_SIZE_49, config.ram_path );
+    if ( err ) {
         ChfGenerate( CHF_ERRNO_SET, __FILE__, __LINE__, errno, CHF_ERROR );
         ChfGenerate( MOD_CHF_MODULE_ID, __FILE__, __LINE__, MOD_E_RAM_SAVE, CHF_ERROR );
         ChfSignal( MOD_CHF_MODULE_ID );
@@ -547,13 +550,11 @@ void Ce2Init49( void )
     /* CE2 always present and write enabled */
     mod_status.hdw.card_status |= ( CE2_CARD_PRESENT | CE2_CARD_WE );
 
-#if 0
     /* card_status changed; update, set MP bit in HST and post
        interrupt request.
     */
-    cpu_status.HST |= HST_MP_MASK;
-    CpuIntRequest(INT_REQUEST_IRQ);
-#endif
+    /* cpu_status.HST |= HST_MP_MASK; */
+    /* CpuIntRequest(INT_REQUEST_IRQ); */
 }
 
 /* .+

commit a9ac604ff80efa783237a409b1859a87915044b8

tracked and fixed a bug I introduced in 97371cf
Inverting conditional is hard and has consequences. Breaking writing
to flash is bad

src/core/flash49.c
@@ -244,7 +244,7 @@ static int StoreData( enum FlashState* state, enum FlashCycle cycle, XAddress ad
         case FLASH_ST_WRITE_DATA_N:
             {
                 int index = address - wb_start;
-                if ( index <= 0 && index < WB_SIZE )
+                if ( index >= 0 && index < WB_SIZE )
                     wb[ index ] = data;
                 else {
                     WARNING( FLASH_CHF_MODULE_ID, FLASH_W_BAD_ADDRESS, *state, cycle, address, data )